b200b43495a64fc6a64f80090aa0cfe81fb72f42
Harshit2528 <harshitdongarwar2003@gmail.com>
2024-06-18 15:40:33 +0530

    harshitjava first commit

---
+++ src/BitwiseDemo.java
@@ -0,0 +1,13 @@
+
+import java.lang.*;
+public class BitwiseDemo
+{
+    public static void main(String[] args)
+
+
+    {
+        int x=10, y=6,z=x&y;
+
+        System.out.println(z);
+    }
+}
---
+++ src/printing.java
@@ -0,0 +1,7 @@
+public class printing {
+    public static void main(String []args){
+        int a= 10, b=20;
+
+      System.out.printf("sum of " +a+" and "+b+" is " +(a+b));
+    }
+}
