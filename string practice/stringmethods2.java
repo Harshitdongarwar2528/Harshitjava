9f06f70c747a6ca51c39a60910621459c7acd8e0
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2024-09-28 13:26:13 +0530

    Add files via upload

---
+++ string practice/stringmethods2.java
@@ -0,0 +1,8 @@
+public class stringmethods2
+ {
+  public static void main (String []args)
+  {
+      String str1="www.udemy.co.in";
+      System.out.println(str1.lastIndexOf("."));
+  }
+}
---
+++ string practice/stringpractice.java
@@ -0,0 +1,11 @@
+public class stringpractice {
+    public static void main(String[] args)
+    {
+
+
+     String str=new String("netbeans");
+     String str2=str.replace('e','M' );
+     System.out.println(str2);
+     System.out.println(str2+"    "+str);
+    }
+}
