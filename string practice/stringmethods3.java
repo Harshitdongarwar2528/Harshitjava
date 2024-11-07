b3779229b95890e1d898d59fc2287040913c49e4
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2024-11-07 18:49:09 +0530

    Delete string practice directory

--- string practice/stringmethods3.java
+++
@@ -1,9 +0,0 @@
-public class stringmethods3 {
-    public static void main (String[]args)
-    {
-        String str1="THE GREAT WALL ";
-        String str2=" of china";
-        System.out.println(str1+str2);
-
-    }
-}
